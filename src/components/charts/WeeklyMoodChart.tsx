6fdf23d55daab605ed1fdf7b660027f6357c8a7d
srivishnavi ch <134829048+chvish-git@users.noreply.github.com>
2025-06-29 01:04:44 +0530

    Fix Cell import error in WeeklyMoodChart

--- src/components/charts/WeeklyMoodChart.tsx
+++ src/components/charts/WeeklyMoodChart.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, Cell } from 'recharts';
 import { format, startOfWeek, addDays, isSameDay } from 'date-fns';
 import { MoodLog } from '../../types/mood';
 
