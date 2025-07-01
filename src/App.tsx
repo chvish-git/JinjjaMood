b279614fd46989a33ba2a66eb4a168f01a36cf6e
srivishnavi ch <134829048+chvish-git@users.noreply.github.com>
2025-07-01 13:07:39 +0530

    Fix Console Errors and Warnings

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,12 @@ function App() {
   return (
     <ThemeProvider>
       <AuthProvider>
-        <Router>
+        <Router
+          future={{
+            v7_startTransition: true,
+            v7_relativeSplatPath: true
+          }}
+        >
           <div className="min-h-screen">
             <Navigation />
             <Routes>
